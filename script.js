ab61dd257a5ba920e9428b5b97981a50c4bb3250
imfrompluto <87384505+imfrompluto@users.noreply.github.com>
2021-09-15 17:59:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let rightarrow = document.getElementById("rightarrow")
 let leftarrow = document.getElementById("leftarrow")
 let distance = 0;
 let slider = document.getElementsByClassName("slider")[0]
+let rightcomment = document.getElementById("rightcomment")
+let leftcomment = document.getElementById("leftcomment")
+let pairs = document.getElementsByClassName("pair")
 console.log(rightarrow, leftarrow);
 rightarrow.onclick = function(){
     console.log(680426586)
@@ -25,3 +28,34 @@ leftarrow.onclick = function(){
     slider.style.transform = "translateX(-"+distance+"%)";
 
 }
+
+rightcomment.onclick = function(){
+    console.log(2936214)
+    let activePair = document.getElementsByClassName("activePair")[0]
+ activePair.classList.remove("activePair")
+ let nextPair = activePair.nextElementSibling;
+ if (nextPair.classList[0] == "sliderButtons"){
+     nextPair = pairs[0]
+ }
+ setTimeout(function(){
+    nextPair.classList.add("activePair") 
+ },300)
+}
+leftcomment.onclick = function(){
+    console.log(2936214)
+    let activePair = document.getElementsByClassName("activePair")[0]
+    activePair.classList.remove("activePair")
+    let previousPair 
+    if (activePair.previousElementSibling){
+        previousPair = activePair.previousElementSibling;
+    }
+    else {
+    previousPair = pairs[2]
+    }
+    setTimeout(function(){
+        previousPair.classList.add("activePair")
+    },300)
+}
+
+
+
